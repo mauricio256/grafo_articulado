5bb90a0a49e7e630beff5a1b936022db4fccc6b0
Maurício Franca <https://github.com/mauricio96franca/grafo_articulado.git>
2023-05-03 20:20:00 -0300

    Postagem do projeto ja em adamento

---
+++ main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void){
+	char nomeArquivo[61];
+	char ext[4] = {".txt"};
+    int matriz[7][7];
+    int i,j;
+    int continua;
+    
+  	do{
+  		system("cls");
+		printf(" Digite o nome do arquivo: ");
+		scanf("%s", nomeArquivo);
+		
+		strcat(nomeArquivo, ext); 	/// a função strcat cocaltena o valor da primeira variavel a segunda.
+		
+	    FILE *file;   ///cria ponteiro para um tipo de dados, padrão do C, do tipo file.
+	    file = fopen( nomeArquivo,"r" ); /* A função fopen(), que está sendo atribuida ao ponteiro do tipo file,
+										    abre o arquivo com o segundo atributo com letra "r" de read. O primeiro 
+											atributo é o caminho+nome+entensão do arquivo. */ 
+											
+	    if( file == NULL ){  //// verifica se o arquivo file pode ser lido
+	    	system("cls");
+	    	printf("\n Erro na leitura do arquivo! \n");
+		}else{ 
+	        for(i=0;i<7;i++){ //// percorre todo o arquivo lendo e atribuido a cada valor de i e j a matriz[][] em linhas e colunas   
+	            for(j=0;j<7;j++){
+	                fscanf(file,"%d",&matriz[i][j]);
+	            }
+	        }
+	   
+			fflush(stdin);  /// limpa o bufer 
+			system("cls");
+		    printf("\n---| Matriz %s: |---\n", nomeArquivo);
+		    printf("\n");
+		
+		    for(i=0;i<7;i++){ //// percorre toda a matriz[][] em linhas e colunas e printa na tela
+		        for(j=0;j<7;j++){
+		            printf(" [%d]",matriz[i][j]);
+		        }
+		        printf("\n");
+		    }
+		}
+   	   	fclose(file); /// fecha o arquivo
+    
+	    printf("\n tecle 'Enter' para digitar um novo arquivo ou tecle 's' se para sair \n");
+	    continua = getch();
+	    
+	}while (continua != 's'); 
+
+	return 0;
+
+}
