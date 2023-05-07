6ebe8d4e7c7db4ba3b0ee55d6ffd272382dfc7d2
Maurício Franca <https://github.com/mauricio96franca/grafo_articulado.git>
2023-05-07 04:00:58 -0300

    atualização do projeto

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ int main(void){
 	char nomeArquivo[61];
 	char ext[4] = {".txt"};
     int matriz[7][7];
+    int vet[2];
     int i,j;
     int continua;
     
@@ -23,7 +24,7 @@ int main(void){
 											
 	    if( file == NULL ){  //// verifica se o arquivo file pode ser lido
 	    	system("cls");
-	    	printf("\n Erro na leitura do arquivo ou arquivo inexistente \n");
+	    	printf("\n Erro na leitura ou arquivo inexistente \n");
 		}else{ 
 	        for(i=0;i<7;i++){ //// percorre todo o arquivo lendo e atribuido a cada valor de i e j a matriz[][] em linhas e colunas   
 	            for(j=0;j<7;j++){
@@ -38,10 +39,40 @@ int main(void){
 		
 		    for(i=0;i<7;i++){ //// percorre toda a matriz[][] em linhas e colunas e printa na tela
 		        for(j=0;j<7;j++){
-		            printf(" [%d]",matriz[i][j]);
+		            printf(" [%d]",matriz[i][j]); /// mostra a matriz na tela
 		        }
 		        printf("\n");
 		    }
+		
+		    
+		    //// pega dois vertice e joga em um vetor
+		    for(i=0;i<7;i++){ 
+		        for(j=0;j<7;j++){   ////// i representa numero de linhas  e j representa numero de colunas 
+					if(i==1 && j==2){
+							vet[0] =  matriz[i][j]; //// atribui 1 vertice a 1° posição do vetor
+							vet[1] =  matriz[j][i]; //// atribui o vertice de ligação a 2° posição do vetor
+					} 		
+		        }
+		    }		
+											/*
+		    											 for(i=0;i<7;i++){ 
+													        for(j=0;j<7;j++){   ////// i representa numero de linhas  e j representa numero de colunas 
+																if(i==1 && j==0){
+																		vet[0] =  matriz[i][j]; //// atribui 1 vertice a 1° posição do vetor
+																} 
+																if(i==0 && j==1){  ////// i representa numero de linhas  e j representa numero de colunas 
+																	vet[1] =  matriz[i][j]; // atribui 1 vertice a 2° posição do vetor
+																}
+													        }
+													    }
+		    										*/
+		    
+		    printf("\n vetor com dois vertices selecionados \n");
+		    for(i=0;i<2;i++){
+				printf(" [%d]", vet[i]); /// mostra o vetor na tela
+			} 
+			
+		    printf("\n");
 		}
    	   	fclose(file); /// fecha o arquivo
     
