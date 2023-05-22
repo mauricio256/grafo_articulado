bdd5b5390dbcfca451bd7bda5465418578480ef6
unknown <mauriciodossantosfranca@hotmail.com>
2023-05-22 20:36:33 -0300

    atualização do projeto

--- main.c
+++ main.c
@@ -6,6 +6,8 @@ int main(void){
 	char nomeArquivo[60];
 	char ext[4] = {".txt"};
     int matriz[4][4];
+    int matriz_sec[4][4];
+    int encontrados[10];
     int i,j;
     int continua;
     
@@ -30,17 +32,65 @@ int main(void){
 	                fscanf(file,"%d",&matriz[i][j]);
 	            }
 	        }   
-	    	
-	    	printf("As ligações de vertices sao:\n"); /// percorre
+	        
+	        
+	        
+	        
+	        
+	        /////////// função boleana
+	        int busca_largura(){
+	        	///// recebe 2 valores e verifica se ha caminho
+			}
+	        
+	        
+	        
+	        
+	        
+	        
+	        
+	        
+	    	 printf("\n -----remove diagonal principal \n"); 
 		 	for(i=0;i<4;i++){
-		 		printf("%d-> ", i+1);
 	            for(j=0;j<4;j++){
-	                if(matriz[i][j] == 1){
-	                	printf("[%d]", j+1);
+	                if(i != j){
+	                	printf("[%d]", matriz[i][j]);
 					}
 	            }
 	            printf("\n");
 	        }
+	        
+	        printf("\n -----remove linha1 \n");
+	        
+	    	for(i=0;i<4;i++){
+	            for(j=0;j<4;j++){
+	                if(i != 0){
+	                	printf("[%d]", matriz[i][j]);
+					}
+	            }
+	            printf("\n");
+	        } 
+	        
+	        printf("\n --- remove linha 2 \n");
+			for(i=0;i<4;i++){
+	            for(j=0;j<4;j++){
+	                if(i != 1){
+	                	printf("[%d]", matriz[i][j]);
+					}
+	            }
+	            printf("\n");
+	        } 
+		  
+	         printf("\n --- remove coluna 3 \n"); 
+			for(i=0;i<4;i++){
+	            for(j=0;j<4;j++){
+	                if(j != 2){
+	                	printf("[%d]", matriz[i][j]);
+					}
+	            }
+	            printf("\n");
+	        } 
+		  
+	        
 			
 					
 		}/// fim do else
