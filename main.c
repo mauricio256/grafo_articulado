afb31154a710cf74cd2eb8cde953054d05ee8bd3
unknown <mauriciodossantosfranca@hotmail.com>
2023-05-27 17:31:27 -0300

    atualizacao feita com tenorio

--- main.c
+++ main.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//#include <>
 
 int main(void){
 	char nomeArquivo[60];
 	char ext[4] = {".txt"};
     int matriz[5][5];
-    int encontrados[10];
+    int encontrados[5];
     int i,j;
     int continua;
+    
+    int tot, ver, indice;
+    
+    bool teste (int x){
+    	bool aux;
+    	aux = false;
+ 		for (i=1; i<= tot; i++){
+ 			if (x = encontrados[i]){
+ 				aux = true;
+			 }
+		 }
+		 return aux;   	
+    	
+	}
+    
+    
     
   	do{
   		system("cls");
@@ -26,12 +43,36 @@ int main(void){
 	    	system("cls");
 	    	printf("\n Erro na leitura ou arquivo inexistente \n");
 		}else{ 
-	        for(i=0;i<5;i++){ //// percorre todo o arquivo lendo e atribuido a cada valor de i e j a matriz[][] em linhas e colunas   
-	            for(j=0;j<5;j++){
+	        for(i=1;i<6;i++){ //// percorre todo o arquivo lendo e atribuido a cada valor de i e j a matriz[][] em linhas e colunas   
+	            for(j=1;j<6;j++){
 	                fscanf(file,"%d",&matriz[i][j]);
 	            }
 	        }  
 
+	        
+	        
+	        tot = 1;
+	        
+	        indice = 0;
+	        encontrados [tot] = 1;
+	        
+	        
+	        do { 
+	        	indice = indice + 1;
+	        	ver = encontrados[indice]
+	        	for (j = 1; j<6; j++){
+	        		if (matriz [ver,j] = 1  and !teste (j)) {
+						tot = tot + 1;
+						encontrados[tot] = j;						
+					}
+	        		
+	        		
+				}
+	        
+	        	
+	        
+			} while (indice != tot);
+	        
 	        
 	        printf("As ligações de vertices sao:\n"); /// percorre
 		 	for(i=0;i<5;i++){
@@ -39,13 +80,19 @@ int main(void){
 	            for(j=0;j<5;j++){
 	                if(matriz[i][j] == 1){
 	                	printf("[%d]", j+1);
+	                	
+	                	if( matriz[i][j] != 0 && matriz[i][j] != j-1 != matriz[i][j] != j){
+	                		encontrados[i] = j+1;
+						}
 	                
 					}
 	            }
 	            printf("\n");	
 			}
 	        
-	        
+	        for(i=0;i<5;i++){
+				printf("[%d]", encontrados[i]);
+			}
 	        
 	        
 	        
